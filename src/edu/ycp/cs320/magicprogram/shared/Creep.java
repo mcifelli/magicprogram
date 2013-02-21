2c55f9df76f68e4359588e58b9b65c41626322f7
mcifelli <mcifelli@ycp.edu>
2013-02-21 16:15:11 -0500

    initial gwt

---
+++ src/edu/ycp/cs320/magicprogram/shared/Creep.java
@@ -0,0 +1,41 @@
+package edu.ycp.cs320.magicprogram.shared;
+
+public class Creep {
+	// Physical attributes
+	private Rectangle body;
+	private Circle range;
+	private boolean dead;
+	
+	// Constructors
+	public Creep(Point topLeft, double size) {
+		setBody(new Rectangle(topLeft, size));
+		setRange(new Circle(getCenter()));
+		dead = false;
+	}
+	
+	// Getters/Setters
+	public Rectangle getBody() {
+		return body;
+	}
+	public void setBody(Rectangle body) {
+		this.body = body;
+	}
+	public Circle getRange() {
+		return range;
+	}
+	public void setRange(Circle range) {
+		this.range = range;
+	}
+	public Point getLocation() {
+		return body.getLocation();
+	}
+	
+	//Methods
+	public void move(Point newPoint) {
+		
+	}
+	
+	public void kill() {
+		dead = true;
+	}
+}
