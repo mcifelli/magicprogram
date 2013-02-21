7356a424c03deb74bc7ad9ea7b19a87d21df5410
Paul Holtz <pholtz@ycp.edu>
2013-02-21 16:31:14 -0500

    added old test classes

--- test/edu/ycp/cs320/magicprogramtest/TestRectangle.java
+++ test/edu/ycp/cs320/magicprogramtest/TestRectangle.java
@@ -1,11 +1,9 @@
 package edu.ycp.cs320.magicprogramtest;
 
-import static junit.framework.Assert.*;
 import org.junit.Before;
 import org.junit.Test;
 
-import edu.ycp.cs320.SHARED.Point;
-import edu.ycp.cs320.SHARED.Rectangle;
+import edu.ycp.cs320.magicprogram.shared.*;
 
 
 public class TestRectangle {
