c880489c72ba746e835ad9f7eaecef43024418be
Paul Holtz <pholtz@ycp.edu>
2013-03-11 22:18:53 -0400

    commmitcommitcomim t

--- magicprogramUI/src/edu/ycp/cs320/magicprogram/client/GameView.java
+++ magicprogramUI/src/edu/ycp/cs320/magicprogram/client/GameView.java
@@ -1,26 +1,26 @@
 package edu.ycp.cs320.magicprogram.client;
 
 import com.google.gwt.canvas.client.Canvas;
+import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.*;
 
 import edu.ycp.cs320.magicprogram.shared.*;
 
-import com.google.gwt.dom.client.Style.Unit;
-
-public class GameView extends Composite implements ISubscriber {
+public class GameView extends Composite {
 	
 	private Game model;
+	public Canvas canvas;
 	
-//	private Canvas canvas;
-//	private Canvas canvas_1;
-	
-	public GameView() {
+	public GameView(FlowPanel panel) {
 		
-		LayoutPanel layoutPanel = new LayoutPanel();
-		initWidget(layoutPanel);
+//		LayoutPanel layoutPanel = new LayoutPanel();
+//		initWidget(layoutPanel);
 		
-		
-		
+		canvas = Canvas.createIfSupported();
+		canvas.setStyleName("h1");
+		panel.add(canvas);
+		canvas.setSize("500px", "500px");
+//		RootLayoutPanel rootLayoutPanel_1 = RootLayoutPanel.get();
 		
 	}
 	
@@ -28,14 +28,25 @@ public class GameView extends Composite implements ISubscriber {
 		this.model = game;
 	}
 	
+	public void start() {
+		Timer timer = new Timer() {
+			@Override
+			public void run() {
+				updateGame();
+				drawCanvas(canvas);
+			}
+		};
+		timer.scheduleRepeating(10);
+	}
+	
 	//update UI
 	public void updateGame() {
-		
+		model.update();
+	}
+	
+	public void drawCanvas(Canvas canvas) {
+		canvas.getContext2d().setFillStyle("#ff0000");
+		canvas.getContext2d().fillRect(20, 20, 80, 40);
 	}
 
-	@Override
-	public void eventOccurred(Object key, IPublisher publisher, Object hint) {
-		// TODO Auto-generated method stub
-		
-	}
 }
--- magicprogramUI/src/edu/ycp/cs320/magicprogram/client/MagicprogramUI.java
+++ magicprogramUI/src/edu/ycp/cs320/magicprogram/client/MagicprogramUI.java
@@ -1,18 +1,10 @@
 package edu.ycp.cs320.magicprogram.client;
 
-import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.dom.client.Style.Unit;
-import com.google.gwt.user.client.ui.LayoutPanel;
+import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RootLayoutPanel;
-import com.google.gwt.user.client.ui.TextBox;
-import com.google.gwt.user.client.ui.Button;
-import com.google.gwt.event.dom.client.ClickHandler;
-import com.google.gwt.event.dom.client.ClickEvent;
-
 import edu.ycp.cs320.magicprogram.shared.*;
-//import com.google.gwt.user.client.ui.TreeImages;
-import com.google.gwt.core.client.GWT;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -21,50 +13,31 @@ public class MagicprogramUI implements EntryPoint {
 	/**
 	 * This is the entry point method.
 	 */
-	private Board board;
-	private Creep creep;
-	private Point spawnPoint;
-	
-	Game game = new Game();
+
 	/**
 	 * @wbp.nonvisual location=118,99
 	 */
 	
-	public MagicprogramUI() {
-		
-	}
-	
 	public void onModuleLoad() {
 		// create a panel
-		LayoutPanel panel = new LayoutPanel();
-		
-		GameView gameView = new GameView();
-		gameView.setModel(game);
+		FlowPanel fpanel = new FlowPanel();
 		
 		
+		Game game = new Game();
 		
+		GameView view = new GameView(fpanel);
+		view.setModel(game);
 		
 		// add your view(s) to the panel
 		//panel.add(yourView);
 		
 		RootLayoutPanel rootLayoutPanel = RootLayoutPanel.get();
-		rootLayoutPanel.add(panel);
-		RootLayoutPanel.get().setWidgetLeftRight(panel, 10.0, Unit.PX, 10.0, Unit.PX);
-		RootLayoutPanel.get().setWidgetTopBottom(panel, 10.0, Unit.PX, 10.0, Unit.PX);
+		rootLayoutPanel.add(fpanel);
+		RootLayoutPanel.get().setWidgetLeftRight(fpanel, 100.0, Unit.PX, 100.0, Unit.PX);
+		RootLayoutPanel.get().setWidgetTopBottom(fpanel, 100.0, Unit.PX, 100.0, Unit.PX);
 		
-		Button creepButton = new Button("New button");
-		creepButton.addClickHandler(new ClickHandler() {
-			public void onClick(ClickEvent event) {
-				creepButtonClicked();
-			}
-		});
-		creepButton.setText("add creep");
-		panel.add(creepButton);
-		panel.setWidgetLeftWidth(creepButton, 325.0, Unit.PX, 81.0, Unit.PX);
-		panel.setWidgetTopHeight(creepButton, 52.0, Unit.PX, 30.0, Unit.PX);
+		view.start();
+		
 	}
-	
-	public void creepButtonClicked() {
-		board.addCreep(creep);
-	}
+
 }
