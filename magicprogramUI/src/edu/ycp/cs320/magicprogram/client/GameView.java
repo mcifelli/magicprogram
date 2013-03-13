b4097108e8e59b2623e15d9e1be4f7317e925dca
Paul Holtz <pholtz@ycp.edu>
2013-03-13 11:45:52 -0400

    added view

--- magicprogramUI/src/edu/ycp/cs320/magicprogram/client/GameView.java
+++ magicprogramUI/src/edu/ycp/cs320/magicprogram/client/GameView.java
@@ -1,26 +1,25 @@
 package edu.ycp.cs320.magicprogram.client;
 
+import com.google.gwt.event.dom.client.*;
+import com.google.gwt.event.shared.GwtEvent;
 import com.google.gwt.canvas.client.Canvas;
 import com.google.gwt.user.client.Timer;
 import com.google.gwt.user.client.ui.*;
 
 import edu.ycp.cs320.magicprogram.shared.*;
 
-public class GameView extends Composite {
-	
+public class GameView {
+//	private static final long serialVersionUID = 1L;
 	private Game model;
 	public Canvas canvas;
 	
 	public GameView(FlowPanel panel) {
 		
-//		LayoutPanel layoutPanel = new LayoutPanel();
-//		initWidget(layoutPanel);
-		
+		//set up game canvas
 		canvas = Canvas.createIfSupported();
 		canvas.setStyleName("h1");
 		panel.add(canvas);
 		canvas.setSize("500px", "500px");
-//		RootLayoutPanel rootLayoutPanel_1 = RootLayoutPanel.get();
 		
 	}
 	
@@ -37,6 +36,12 @@ public class GameView extends Composite {
 			}
 		};
 		timer.scheduleRepeating(10);
+		
+		MouseDownHandler mouseDown;
+		HasAllMouseHandlers handler;
+		handler.addMouseDownHandler(mouseDown);
+		
+		
 	}
 	
 	//update UI
@@ -45,6 +50,9 @@ public class GameView extends Composite {
 	}
 	
 	public void drawCanvas(Canvas canvas) {
+		for(int i = 0; i < model.getBoard().getCreeps().size(); i++) {
+			canvas.getContext2d().fillRect(model.getBoard().getCreeps().get(i).getCenter().getX(), model.getBoard().getCreeps().get(i).getCenter().getY(), model.getBoard().getCreeps().get(i).getBody().getWidth(), model.getBoard().getCreeps().get(i).getBody().getHeight());
+		}
 		canvas.getContext2d().setFillStyle("#ff0000");
 		canvas.getContext2d().fillRect(20, 20, 80, 40);
 	}
