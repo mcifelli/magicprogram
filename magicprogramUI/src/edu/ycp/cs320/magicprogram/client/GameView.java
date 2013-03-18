f2e991a55e310605b2a7c6c4696e63321c923b84
Paul Holtz <pholtz@ycp.edu>
2013-03-18 10:55:16 -0400

    gameview update

--- magicprogramUI/src/edu/ycp/cs320/magicprogram/client/GameView.java
+++ magicprogramUI/src/edu/ycp/cs320/magicprogram/client/GameView.java
@@ -10,7 +10,7 @@ import com.google.gwt.user.client.ui.*;
 
 import edu.ycp.cs320.magicprogram.shared.*;
 
-public class GameView implements HasAllMouseHandlers {
+public class GameView {
 //	private static final long serialVersionUID = 1L;
 	private Game model;
 	public Canvas canvas;
@@ -40,28 +40,15 @@ public class GameView implements HasAllMouseHandlers {
 		};
 		timer.scheduleRepeating(10);
 		
+		model.addCreep();
+		
 		canvas.addMouseDownHandler(new MouseDownHandler() {
 			@Override
 			public void onMouseDown(MouseDownEvent event) {
 				// TODO Auto-generated method stub
 				clickHandle(event);
-//				drawBlue(canvas);
 			}
 		});
-		
-//	    canvas.addTouchMoveHandler(new TouchMoveHandler() {
-//	        public void onTouchMove(TouchMoveEvent event) {
-//	          event.preventDefault();
-//	          if (event.getTouches().length() > 0) {
-//	            Touch touch = event.getTouches().get(0);
-//	            mouse.setX(touch.getRelativeX(canvas.getElement()));
-//	            mouse.setY(touch.getRelativeY(canvas.getElement()));
-//	          }
-//	          event.preventDefault();
-//	          System.out.println("hey");
-//	          drawBlue(canvas);
-//	        }
-//	    });
 		
 		
 	}
@@ -70,11 +57,12 @@ public class GameView implements HasAllMouseHandlers {
 	protected void clickHandle(MouseDownEvent event) {
 		mouse.setX(event.getClientX());
 		mouse.setY(event.getClientY());
-//		model.getBoard().addCreep(new Creep(new Rectangle(new Point(mouse.getX(), mouse.getY()), Game.CSIZE, Game.CSIZE), 10, 2, new Point(mouse.getX(), mouse.getY())));
+//		model.getCreeps().get(0).
 	}
 	
 	//update UI
 	public void updateGame() {
+		canvas.getContext2d().restore();
 		model.update();
 	}
 	
@@ -84,56 +72,14 @@ public class GameView implements HasAllMouseHandlers {
 	}
 	
 	public void drawCanvas(Canvas canvas) {
-		for(int i = 0; i < model.getBoard().getCreeps().size(); i++) {
-			if(model.getBoard().getCreeps().size() > 0) {
+		for(int i = 0; i < model.getCreeps().size(); i++) {
+			if(model.getCreeps().size() > 0) {
 				canvas.getContext2d().setFillStyle("Blue");
-				canvas.getContext2d().fillRect(model.getBoard().getCreeps().get(i).getCenter().getX(), model.getBoard().getCreeps().get(i).getCenter().getY(), model.getBoard().getCreeps().get(i).getBody().getWidth(), model.getBoard().getCreeps().get(i).getBody().getHeight());
+				canvas.getContext2d().fillRect(model.getCreeps().get(i).getCenter().getX(), model.getCreeps().get(i).getCenter().getY(), model.getCreeps().get(i).getBody().getWidth(), model.getCreeps().get(i).getBody().getHeight());
 			}
 		}
 //		canvas.getContext2d().setFillStyle("#ff0000");
 //		canvas.getContext2d().fillRect(20, 20, 80, 40);
-	}
-
-	@Override
-	public HandlerRegistration addMouseDownHandler(MouseDownHandler handler) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void fireEvent(GwtEvent<?> event) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public HandlerRegistration addMouseUpHandler(MouseUpHandler handler) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public HandlerRegistration addMouseOutHandler(MouseOutHandler handler) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public HandlerRegistration addMouseOverHandler(MouseOverHandler handler) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public HandlerRegistration addMouseMoveHandler(MouseMoveHandler handler) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public HandlerRegistration addMouseWheelHandler(MouseWheelHandler handler) {
-		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
